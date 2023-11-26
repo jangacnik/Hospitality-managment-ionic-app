f3cbebf0df14701631c30f331691452dea00ca02
Mitja Polner <mitja@maked-energea.si>
2023-11-26 12:49:51 +0100

    Spremembe: - Dodan logotip na vse strani - Narejeni improvementi pri izgledu strani domov

--- ResortPlatform/src/app/interceptor/jwt.interceptor.ts
+++ ResortPlatform/src/app/interceptor/jwt.interceptor.ts
@@ -1,16 +1,16 @@
-import {Injectable, OnInit} from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor
+  HttpInterceptor,
 } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {StorageService} from "../service/storage.service";
+import { StorageService } from '../service/storage.service';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor, OnInit {
-  private jwt = "";
+  private jwt = '';
   constructor(private storage: StorageService) {
     this.storage.jwtChangedSub.subscribe((jwt) => {
       this.jwt = jwt;
@@ -18,24 +18,26 @@ export class JwtInterceptor implements HttpInterceptor, OnInit {
   }
 
   // @ts-ignore
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-        if(req.headers.get("Authorization")) {
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    if (req.headers.get('Authorization')) {
+      return next.handle(req);
+    }
+    if (!this.jwt) {
+      return next.handle(req);
+    }
 
-          return next.handle(req);
-        }
-        if (!this.jwt) {
-          return next.handle(req);
-        }
-
-        const req1 = req.clone({
-          headers: req.headers.append('Authorization', `Bearer ${this.jwt}`),
-        });
-        return next.handle(req1);
+    const req1 = req.clone({
+      headers: req.headers.append('Authorization', `Bearer ${this.jwt}`),
+    });
+    return next.handle(req1);
   }
 
   ngOnInit(): void {
     this.storage.jwtChangedSub.subscribe((jwt) => {
-    this.jwt = jwt;
-  });
+      this.jwt = jwt;
+    });
   }
 }
