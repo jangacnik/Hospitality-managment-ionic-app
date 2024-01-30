fe328a6d82a92f4cc29ea664774f47e90ebf5a3d
Jan Gacnik <nqmrnr7xgg@privaterelay.appleid.com>
2024-01-30 17:48:39 +0100

    Changed app name

--- ResortPlatform/capacitor.config.ts
+++ ResortPlatform/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'ResortPlatform',
+  appId: 'io.hospify',
+  appName: 'Vestlia',
   webDir: 'www',
   server: {
     androidScheme: 'https'
